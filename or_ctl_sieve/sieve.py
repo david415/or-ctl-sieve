42efaa847ebc70b0848ce8950440b787e41f048a
David Stainton <dstainton415@gmail.com>
2015-10-21 12:07:45 +0200

    Add replacements tube for client side

--- or_ctl_sieve/sieve.py
+++ or_ctl_sieve/sieve.py
@@ -8,6 +8,18 @@ from tubes.tube import tube, series
 
 
 @tube
+class replace_tube(object):
+    def __init__(self, replacements):
+        self.replacements = replacements
+
+    def received(self, line):
+        if line in self.replacements:
+            print "replacing %r with %r" % (line, self.replacements[line])
+            yield self.replacements[line]
+        else:
+            yield line
+
+@tube
 class filter_tube(object):
     def __init__(self, allowed, allowed_prefixes):
         if len(allowed) == 0 and len(allowed_prefixes) == 0:
@@ -33,14 +45,16 @@ class filter_tube(object):
             print "allowed: %r" % (line,)
             yield line
 
+
 class proxyOrSieve(object):
 
-    def __init__(self, proxyEndpoint, client_allowed, client_allowed_prefixes, server_allowed, server_allowed_prefixes):
+    def __init__(self, proxyEndpoint, client_allowed, client_allowed_prefixes, server_allowed, server_allowed_prefixes, client_replacements):
         self.proxyEndpoint = proxyEndpoint
         self.client_allowed = client_allowed
         self.client_allowed_prefixes = client_allowed_prefixes
         self.server_allowed = server_allowed
         self.server_allowed_prefixes = server_allowed_prefixes
+        self.client_replacements = client_replacements
 
     def tube_factory(self, listeningFount, listeningDrain):
         """cross-connects the client endpoint self.proxyEndpoint,
@@ -48,8 +62,9 @@ class proxyOrSieve(object):
         """
         def outgoingTubeFactory(connectingFount, connectingDrain):
             client_sieve = filter_tube(self.client_allowed, self.client_allowed_prefixes)
+            client_replace = replace_tube(self.client_replacements)
             server_sieve = filter_tube(self.server_allowed, self.server_allowed_prefixes)
-            proxy_client_sieve = series(bytesToLines(), client_sieve, linesToBytes())
+            proxy_client_sieve = series(bytesToLines(), client_replace, client_sieve, linesToBytes())
             proxy_server_sieve = series(bytesToLines(), server_sieve, linesToBytes())
             listeningFount.flowTo(proxy_client_sieve).flowTo(connectingDrain)
             connectingFount.flowTo(proxy_server_sieve).flowTo(listeningDrain)        
